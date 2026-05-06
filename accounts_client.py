f5e0f1910f27c1c5162f6d5ee0a86bcbcd30b3ae
souf <arhlabi.soufiane02@gmail.com>
2026-05-06 12:34:02 +0800

    custom mcp client with connection per method for tools and resources

---
+++ accounts_client.py
@@ -0,0 +1,36 @@
+import mcp
+from mcp.client.stdio import stdio_client
+from mcp import StdioServerParameters
+from agents import FunctionTool
+import json
+
+params = StdioServerParameters(command="uv", args=["run", "accounts_server.py"], env=None)
+
+## list account tools
+async def list_accounts_tools():
+      async with stdio_client(params) as streams:
+            async with mcp.ClientSession(*streams) as session:
+                  await session.initialize()
+                  tools_result = await session.list_tools()
+                  return tools_result.tools
+
+async def call_accounts_tool(tool_name: str, tool_args: dict):
+      async with stdio_client(params) as streams:
+            async with mcp.ClientSession(*streams) as session:
+                  await session.initialize()
+                  result = await session.call_tool(tool_name, tool_args)
+                  return result
+
+async def read_accounts_resource(name):
+    async with stdio_client(params) as streams:
+        async with mcp.ClientSession(*streams) as session:
+            await session.initialize()
+            result = await session.read_resource(f"accounts://accounts_server/{name}")
+            return result.contents[0].text
+        
+async def read_strategy_resource(name):
+    async with stdio_client(params) as streams:
+        async with mcp.ClientSession(*streams) as session:
+            await session.initialize()
+            result = await session.read_resource(f"accounts://strategy/{name}")
+            return result.contents[0].text
--- lab2.py
+++ lab2.py
@@ -13,18 +13,19 @@ you are able to manage an account to a client and anser question about the accou
 async def main():
       ## re
       async with MCPServerStdio(params=account_params, client_session_timeout_seconds=60) as account_mcp:
+            print(dir(account_mcp))
             list_tools = await account_mcp.list_tools()
-            agent = Agent(
-                name="Account bot",
-                instructions=insttructions,
-                model="gpt-4.1-mini",
-                mcp_servers=[account_mcp]
-            )
-            with trace("account_bot"):
-                  result = await Runner.run(
-                    agent,
-                    "My name is John and my account is  under the name: John Doe, i want to know my balance and holdings",
-                    max_turns=20)
-                  print(result.final_output)
+            # agent = Agent(
+            #     name="Account bot",
+            #     instructions=insttructions,
+            #     model="gpt-4.1-mini",
+            #     mcp_servers=[account_mcp]
+            # )
+            # with trace("account_bot"):
+            #       result = await Runner.run(
+            #         agent,
+            #         "My name is John and my account is  under the name: John Doe, i want to know my balance and holdings",
+            #         max_turns=20)
+            #       print(result.final_output)
 
 asyncio.run(main())
